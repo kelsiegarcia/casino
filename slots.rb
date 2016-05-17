31ee946faf21b72f6d87ac7746519752d382c9c3
Lindsey Font <lindseyfont@Lindseys-MBP.nathan.fifefamily.net>
2016-05-17 01:56:37 -0600

    Basic structure

--- slots.rb
+++ slots.rb
@@ -1,1 +1,11 @@
-module 
+require 'pry'
+require_relative 'player'
+
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file }
+
+class Slots
+	include Mechanics
+	include Player
+
+
+end 
