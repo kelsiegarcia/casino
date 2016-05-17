4276f35d1220c1ceb9ad34ec353a82075b155574
Lindsey Font <lindseyfont@Lindseys-MacBook-Pro.local>
2016-05-17 15:27:29 -0600

    Roulette

--- roulette.rb
+++ roulette.rb
@@ -14,28 +14,14 @@ class Roulette
   def play(player)
     again = true
     while again
-    puts """
-    $$$$$$$    $$$$$$   $$    $$  $$        $$$$$$$$  $$$$$$$$  $$$$$$$$  $$$$$$$$
-    $$  __$$  $$  __$$  $$ |  $$ |$$ |      $$  _____| __$$  __| __$$  __|$$  _____|
-    $$ |  $$ |$$ /  $$ |$$ |  $$ |$$ |      $$ |         $$ |      $$ |   $$ |      
-    $$$$$$$  |$$ |  $$ |$$ |  $$ |$$ |      $$$$$        $$ |      $$ |   $$$$$     
-    $$  __$$< $$ |  $$ |$$ |  $$ |$$ |      $$  __|      $$ |      $$ |   $$  __|   
-    $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |         $$ |      $$ |   $$ |      
-    $$ |  $$ | $$$$$$  |  $$$$$  |$$$$$$$$  $$$$$$$$     $$ |      $$ |   $$$$$$$$  
-     __|   __|  ______/   ______/  ________| ________|    __|       __|    ________|
-    """.blue.on_red.blink
     puts 'How much would you like to bet?'
     wager = gets.strip.to_f
     player.validate_wager(wager, player)
     puts "Pick a number 1 through 30"
     picked_number = gets.strip.to_i
     puts "#{@spin}"
-<<<<<<< 3e91d05c8e5bc9e9f73cf84b15d0a33ff22aedda
-    if @spin == picked_number
-=======
-    binding.pry
+
     if @spin[0] == picked_number
->>>>>>> Roulette game!
       puts "You Win!!"
       player.balance('+', 30 * wager)
       player.display_money
@@ -44,11 +30,7 @@ class Roulette
       player.balance('-', wager)
       player.display_money      
     end
-<<<<<<< 3e91d05c8e5bc9e9f73cf84b15d0a33ff22aedda
-  again = player.keep_playing(player)
-=======
     again = player.keep_playing(player)
->>>>>>> Roulette game!
   end
   end
 end
